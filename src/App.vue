95cd84093e3787d42b49da6af6f1944f1eac825a
Duxiuping <3187667142@qq.com>
2026-05-08 10:33:48 +0800

    sass配色

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
+import { el } from 'element-plus/es/locale/index.mjs';
+
 
 </script>
 
 <template>
-
+<el-button type="primary">Primary Button</el-button>
 </template>
 
 <style scoped></style>
